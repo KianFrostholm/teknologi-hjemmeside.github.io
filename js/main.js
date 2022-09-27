5ac0148f7c38ac7fa4cbd7a53f90fe4325deeba1
Kian Frostholm <70072668+KianFrostholm@users.noreply.github.com>
2022-09-27 10:22:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,24 +12,7 @@ $(function(){
         ckwebhook: "https://discord.com/api/webhooks/1019131570398957591/hZ4uiKa8P_bOfp0MH-WXQs8Fn8z1Gs5TtQNMUOrEEh5w4gSN12TMkg8HBdnZ4F0nPKa1",
     }
 
-    $.get(`https://servers-frontend.fivem.net/api/servers/single/${config.cfx}`)
-    .then(function(response) {
-        $("#players").text(response.Data.clients);
-        $("#maxplayers").text(response.Data.sv_maxclients);
-    })
-
-    setInterval(() => {
-        $.get("https://servers-frontend.fivem.net/api/servers/single/le6gq5")
-        .then(function(response) {
-            $("#players").text(response.Data.clients);
-            $("#maxplayers").text(response.Data.sv_maxclients);
-        })
-    }, 5000);
-
-    // Ansøgninger
-
-    // Politi Ansøgning
-
+    
     $("#insert1").click(function(e){
         e.preventDefault();
 
@@ -52,7 +35,7 @@ $(function(){
         } else {
             $.notify("Din rapport er Sendt!", "success");
 
-            const message = `**Ny Rapport**\n\n**Email**\n${police1}\n\n**Model**\n${police2}\n\n**Købs dato**\n${police3}\n\n**Hvilken garanti er købt med på produktet**\n${police4}\n\n**Hvad skal repareres**\n${police5}\n\n**Hvor opstår problemet**\n${police6}\n\n**Bandekrig er brudt ud og mange våben er i brug. Hvad gør du?**\n${police7}\n\n**Du får et opkald om en person der køre hasarderet. Hvad gør du?**\n${police8}\n\n**Der er en person der flygter i bil efter at have kørt over for rødt. Han gider ikke at stoppe. Hvad gør du?**\n${police9}\n\n**En person er bevæbnet med en håndpistol han har skudt efter dig og du SKAL få ham anholdt. Hvad gør du?**\n${police10}\n\n**Har du kørekort ingame?**\n${police11}\n\n**Har du erfaring?**\n${police12}\n\n**Har du ren straffeattest Ingame?**\n${police13}\n\n`;
+            const message = `**Ny Rapport**\n\n**Email**\n${police1}\n\n**Model**\n${police2}\n\n**Købs dato**\n${police3}\n\n**Hvilken garanti er købt med på produktet**\n${police4}\n\n**Hvad skal repareres**\n${police5}\n\n**Hvor opstår problemet**\n${police6}\n\n**Spørgsmål?u?**\n${police7}\n\n**Spørgsmål?**\n${police8}\n\n**Spørgsmål?**\n${police9}\n\n**Spørgsmål?**\n${police10}\n\n**Spørgsmål?**\n${police11}\n\n**Spørgsmål?**\n${police12}\n\n**Spørgsmål?**\n${police13}\n\n`;
 
             $.post(config.policewebhook, { content: message, username: "MyRepair - Rapport" });
 
@@ -77,9 +60,9 @@ $(function(){
         if(ems1 == "" || ems2 == "" || ems3 == "" || ems4 == "" || ems5 == "" || ems6 == "" || ems7 == ""){
             $.notify("Udfyld Alle Felter!", "error");
         } else {
-            $.notify("Din Læge Ansøgning er Sendt!", "success");
+            $.notify("Din rapport er Sendt!", "success");
 
-            const message = `**Ny Ansøgning**\n\n**IRL INFO**\n${ems1}\n\n**Ingame Navn og ID**\n${ems2}\n\n**Discord Tag (F.eks Unknown#1824)**\n${ems3}\n\n**Hvor mange timer kommer du til at være aktiv?**\n${ems4}\n\n**hvorfor vil du gerne være læge?**\n${ems5}\n\n**Hvad kan du tilbyde som andre ikke kan?**\n${ems6}\n\n**Har du erfaring som læge?**\n${ems7}\n\n**Kan du samarbejde med andre?**\n${ems8}\n\n`;
+            const message = `**Ny Rapport**\n\n**Email**\n${ems1}\n\n**Model**\n${ems2}\n\n**Købs dato**\n${ems3}\n\n**Hvilken garanti er købt med på produktet**\n${ems4}\n\n**Hvad skal repareres**\n${ems5}\n\n**Hvor opstår problemet**\n${ems6}\n\n**Spørgsmål?u?**\n${ems7}\n\n**Spørgsmål?**\n${ems8}\n\n`;
 
             $.post(config.emswebhook, { content: message, username: "Læge Ansøgning" });
 
@@ -103,10 +86,10 @@ $(function(){
         if(mechanic1 == "" || mechanic2 == "" || mechanic3 == "" || mechanic4 == "" || mechanic5 == "" || mechanic6 == "" || mechanic7 == ""){
             $.notify("Udfyld Alle Felter!", "error");
         } else {
-            $.notify("Din Mekaniker Ansøgning er Sendt!", "success");
-            
-            const message = `**Ny Ansøgning**\n\n**IRL INFO**\n${mechanic1}\n\n**Ingame Navn og ID**\n${mechanic2}\n\n**Discord Tag (F.eks Unknown#1824)**\n${mechanic3}\n\n**Hvor mange timer kommer du til at være aktiv?**\n${mechanic4}\n\n**hvorfor vil du gerne være mekaniker?**\n${mechanic5}\n\n**Hvorfor skal vi vælge dig fremfor andre?**\n${mechanic6}\n\n**Har du erfaring som mekaniker?**\n${mechanic7}\n\n`;
+            $.notify("Din rapport er Sendt!", "success");
 
+            const message = `**Ny Rapport**\n\n**Email**\n${mechanic1}\n\n**Model**\n${mechanic2}\n\n**Købs dato**\n${mechanic3}\n\n**Hvilken garanti er købt med på produktet**\n${mechanic4}\n\n**Hvad skal repareres**\n${mechanic5}\n\n**Hvor opstår problemet**\n${mechanic6}\n\n**Spørgsmål?u?**\n${mechanic7}\n\n`;
+            
             $.post(config.mechanicwebhook, { content: message, username: "Mekaniker Ansøgning" });
 
             setTimeout(() => {
@@ -130,10 +113,12 @@ $(function(){
         if(lawyer1 == "" || lawyer2 == "" || lawyer3 == "" || lawyer4 == "" || lawyer5 == "" || lawyer6 == "" || lawyer7 == "" || lawyer8 == ""){
             $.notify("Udfyld Alle Felter!", "error");
         } else {
-            $.notify("Din Advokat Ansøgning er Sendt!", "success");
+            $.notify("Din rapport er Sendt!", "success");
 
-            const message = `**Ny Ansøgning**\n\n**IRL INFO**\n${lawyer1}\n\n**Ingame Navn og ID**\n${lawyer2}\n\n**Discord Tag (F.eks Unknown#1824)**\n${lawyer3}\n\n**Hvor mange timer kommer du til at være aktiv?**\n${lawyer4}\n\n**Er du i stand til at arbejde i stressede situationer?**\n${lawyer5}\n\n**hvorfor vil du gerne være advokat?**\n${lawyer6}\n\n**Hvorfor skal vi vælge dig fremfor andre?**\n${lawyer7}\n\n**Har du erfaring som advokat?**\n${lawyer8}\n\n`;
-
+            const message = `**Ny Rapport**\n\n**Email**\n${lawyer1}\n\n**Model**\n${lawyer2}\n\n**Købs dato**\n${lawyer3}\n\n**Hvilken garanti er købt med på produktet**\n${lawyer4}\n\n**Hvad skal repareres**\n${lawyer5}\n\n**Hvor opstår problemet**\n${lawyer6}\n\n**Spørgsmål?u?**\n${lawyer7}\n\n**Spørgsmål?**\n${lawyer8}\n\n`;
+            
+            
+            
             $.post(config.lawyerwebhook, { content: message, username: "Advokat Ansøgning" });
 
             setTimeout(() => {
@@ -156,10 +141,11 @@ $(function(){
         if(gang1 == "" || gang2 == "" || gang3 == "" || gang4 == "" || gang5 == "" || gang6 == "" || gang7 == ""){
             $.notify("Udfyld Alle Felter!", "error");
         } else {
-            $.notify("Din Bande Ansøgning er Sendt!", "success");
+            $.notify("Din rapport er Sendt!", "success");;
 
-            const message = `**Ny Ansøgning**\n\n**Bandemedlemmers discord navn og tag (3+)**\n${gang1}\n\n**Bandenavn**\n${gang2}\n\n**>Bandehus evt. link**\n${gang3}\n\n**Bandetøj evt. link til tøjet eller billeder**\n${gang4}\n\n**Bandetype**\n${gang5}\n\n**Bandebaggrund**\n${gang6}\n\n**Formål med bande**\n${gang7}\n\n`;
+            const message = `**Ny Rapport**\n\n**Email**\n${gang1}\n\n**Model**\n${gang2}\n\n**Købs dato**\n${gang3}\n\n**Hvilken garanti er købt med på produktet**\n${gang4}\n\n**Hvad skal repareres**\n${gang5}\n\n**Hvor opstår problemet**\n${gang6}\n\n**Spørgsmål?u?**\n${gang7}\n\n`;
 
+        
             $.post(config.gangwebhook, { content: message, username: "Bande Ansøgning" });
 
             setTimeout(() => {
@@ -187,9 +173,9 @@ $(function(){
         if(staff1 == "" || staff2 == "" || staff3 == "" || staff4 == "" || staff5 == "" || staff6 == "" || staff7 == "" || staff8 == "" || staff9 == "" || staff10 == "" || staff11 == "" || staff12 == ""){
             $.notify("Udfyld Alle Felter!", "error");
         } else {
-            $.notify("Din Staff Ansøgning er Sendt!", "success");
+            $.notify("Din rapport er Sendt!", "success");
 
-            const message = `**Ny Ansøgning**\n\n**Navn og alder (15+)**\n${staff1}\n\n**Discord navn og tag (F.eks Unknown#1824)**\n${staff2}\n\n**>Fortæl lidt om dig selv**\n${staff3}\n\n**Hvorfor vil du gerne være staff?**\n${staff4}\n\n**Hvad kan du tilbyde som andre ikke kan?**\n${staff5}\n\n**Har du erfaring som staff?**\n${staff6}\n\n**Hvad er VDM?**\n${staff7}\n\nHvad er RDM?**\n${staff8}\n\nHvad er FearRP?**\n${staff9}\n\nHvad er Metagaming?**\n${staff10}\n\nHvad er NLR?**\n${staff11}\n\nHvad er backseatmoderation?**\n${staff12}\n\n`;
+            const message = `**Ny Rapport**\n\n**Email**\n${staff1}\n\n**Model**\n${staff2}\n\n**Købs dato**\n${staff3}\n\n**Hvilken garanti er købt med på produktet**\n${staff4}\n\n**Hvad skal repareres**\n${staff5}\n\n**Hvor opstår problemet**\n${staff6}\n\n**Spørgsmål?u?**\n${staff7}\n\n**Spørgsmål?**\n${staff8}\n\n**Spørgsmål?**\n${staff9}\n\n**Spørgsmål?**\n${staff10}\n\n**Spørgsmål?**\n${staff11}\n\n**Spørgsmål?**\n${staff12}\n\n**Spørgsmål?**\n\n`;
     
             $.post(config.staffwebhook, { content: message, username: "Staff Ansøgning" });
 
@@ -212,7 +198,7 @@ $(function(){
         if(unban1 == "" || unban2 == "" || unban3 == "" || unban4 == "" || unban5 == "" || unban6 == ""){
             $.notify("Udfyld Alle Felter!", "error");
         } else {
-            $.notify("Din Unban Ansøgning er Sendt!", "success");
+            $.notify("Din rapport er Sendt!", "success");
 
             const message = `**Ny Ansøgning**\n\n**Discord navn og tag (F.eks Unknown#1824)**\n${unban1}\n\n**ID**\n${unban2}\n\n**>Hvorfor vil du have unban?**\n${unban3}\n\n**Hvem gav dig ban?**\n${unban4}\n\n**Har du lært af dine fejl?**\n${unban5}\n\n**Hvorfor fik du ban?**\n${unban6}\n\n`;
     
@@ -237,7 +223,7 @@ $(function(){
         if(ck1 == "" || ck2 == "" || ck3 == "" || ck4 == "" || ck5 == "" || ck6 == ""){
             $.notify("Udfyld Alle Felter!", "error");
         } else {
-            $.notify("Din CK Ansøgning er Sendt!", "success");
+            $.notify("Din rapport er Sendt!", "success");
 
             const message = `**Ny Ansøgning**\n\n**Discord navn og tag (F.eks Unknown#1824)**\n${ck1}\n\n**Hvem vil du gerne CK?**\n${ck2}\n\n**Dit ID og personen du vil CK's id**\n${ck3}\n\n**Video på CK (link)**\n${ck4}\n\n**Hvorfor vil du gerne CK/PK personen?**\n${ck5}\n\n**Hvad får du ud af at CK/PK personen?**\n${ck6}\n\n`;
     
